40c885f7cc8ddf763e3ffb5d8880b0e42c36767c
lokmanhossen678 <lokmanhossen678@gmail.com>
2025-04-16 18:32:55 +0600

    added error

--- src/component/controldField/ControldField.jsx
+++ src/component/controldField/ControldField.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const ControldField = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [password, setPassword] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("submited");
+  }
+  const handlePaswordOnchange = ()=>{
+
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input type="email" name="Email" id="" placeholder="Email" required />
+        <br />
+        <input type="password" name="Password" id="" placeholder="Password" defaultValue={password} onChange={handlePaswordOnchange} required/>
+        <br />
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
 };
 
 export default ControldField;
